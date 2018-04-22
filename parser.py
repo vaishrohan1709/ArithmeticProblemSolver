9fc1328be6db8e7aa7e693747826486ebe7847d1
Janice Darling <janicedarling1@gmail.com>
2018-04-22 19:53:45 -0400

    Finish get_prep_phrase(...)

--- parser.py
+++ parser.py
@@ -12,6 +12,12 @@ def parse(question):
     sentences = tokenizer.tokenize(question)
     print(sentences)
     return_sentence = ''
+    v1 = ''
+    v2 = ''
+    p1 = ''
+    p2 = ''
+    prp1 = ''
+    prp2 = ''
     for sentence in sentences:
         conj = ' '
         # Switch on specific conjunctions from dataset
@@ -32,65 +38,113 @@ def parse(question):
             # Create a file config.py and and set path = to path to stanford-corenlp-full-2018-02-27
             nlp = StanfordCoreNLP(config.path, memory='8g')
 
-            # Tokenize each segment
-            left_segment_tokens = nlp.word_tokenize(left_segment)
-            right_segment_tokens = nlp.word_tokenize(right_segment)
+            if is_verb(left_segment, nlp):
+                v1 = get_verb_phrase(left_segment, nlp).rstrip()
+                entity = nlp.ner(left_segment.split(v1)[0])
+                if entity[0][1] == "TITLE" or entity[0][1] == "PERSON":
+                    p1 = entity[0][0]
 
-            '''
-            if not isVerb(left_segment, nlp):
-                return_sentence = return_sentence + sentence + ' '
-                continue
+                if not is_verb(right_segment, nlp):
+                    v2 = v1
+                    p2 = p1
 
-            # TODO: get verb phrase, is prep
+                else:
+                    v2 = get_verb_phrase(right_segment, nlp)
+                    entity = nlp.ner(right_segment.split(v1)[0])
+                    if entity[0][1] == "TITLE" or entity[0][1] == "PERSON":
+                        p2 = entity[0][0]
+                if is_prep(left_segment, nlp):
+                    prp1 = get_prep_phrase(left_segment, nlp)
+                if is_prep(right_segment, nlp):
+                    prp2 = get_prep_phrase(right_segment, nlp)
+                if prp1 == '' and not prp2.startswith('for'):
+                    prp1 = prp2
+            elif not is_verb(left_segment, nlp):
+                pass
+            L2 = right_segment.replace(v2, "")
+            L2 = L2.replace(prp2, "")
 
-            nlp.close()
-            else:
-            print("Here")
-            '''
-
-            if isVerb(left_segment, nlp):
-                v1 = getVerbPhrase(left_segment, nlp)
-                left_entity = nlp.ner(left_segment.split(v1)[0])
-                if left_entity[0][1] == "TITLE" or left_entity[0][1] == "PERSON":
-                    p1 = left_entity[0][0]
-
-
-
-            nlp.close()
+        print(left_segment)
+        print(L2)
+        print(v1, p1, v2, p2)
+        nlp.close()
     return return_sentence
 
 
-def isVerb(segment, nlp):
+def is_verb(segment, nlp):
     """
     :param segment: sentence segment to check for verbs
     :param nlp: stanfordcorenlp engine
     :return: truth value of verb presence in phrase
     """
+    for word in segment.split():
+        tag = nlp.pos_tag(word)[0][1]
+        if tag.find('VB') != -1:
+            return True
+
+    '''
     index = 1
     for word in segment.split():
         tag = nlp.pos_tag(word)[0][1]
+        if 'VBG' in tag:
+            index += 1
+            continue
 
         if tag.find('VB') != -1 and segment.index(word) != 0:
-            if not str(segment[segment.index(word) - 1]).find('TO') != 1 and index < len(segment):
+            if str(segment[segment.index(word) - 1]).find('TO') == -1 and index < len(segment):
                 if not isVerb(segment[index:len(segment) - 1], nlp):
                     return True
-        if tag.find('VB') and segment.index(word) == 0:
+        if tag.find('VB') != -1 and segment.index(word) == 0:
             if len(segment) > 1 and index < len(segment) and not isVerb(segment[index:len(segment) - 1], nlp):
                 return True
             if len(segment) == 1:
                 return True
         index += 1
+    
+    return False
+    '''
     return False
 
 
-def getVerbPhrase(segment, nlp):
+# TODO: Rewrite function to return verb phrase not just verb
+def get_verb_phrase(segment, nlp):
     for word in segment.split():
         pos = nlp.pos_tag(word)[0]
-        if "VB" in pos[1]:
+        if pos[1].find("VB") != -1:
             return pos[0]
 
 
+def is_prep(segment, nlp):
+    for word in segment.split():
+        tag = nlp.pos_tag(word)[0][1]
+        if tag.find('IN') != -1 or tag.find('TO') != -1:
+            return True
+    return False
 
 
 
+def get_prep_phrase(segment, nlp):
+    cross_prep = False
+    cross_verb = False
+    prepPhrase = ""
+    for word in segment.split():
+        tag = nlp.pos_tag(word)[0][1]
+        if tag.find("VB") != -1:
+            cross_verb = True
+        if is_verb(segment, nlp) and not cross_verb:
+            continue
+        if not cross_prep and tag.find("IN") != -1 and not word == "by" or tag.find("TO") != -1:
+            cross_prep = True
+            prepPhrase = prepPhrase + word + " "
+            continue
+        if cross_prep and tag.find("IN") != -1  and not word == "by" or tag.find("TO") != -1:
+            prepPhrase = ""
+            prepPhrase = prepPhrase + word + " "
+            continue
+        elif cross_prep:
+            prepPhrase = prepPhrase + word + " "
 
+    prepPhrase = prepPhrase.replace(" .", ".")
+    prepPhrase = prepPhrase.replace(" ,", ",")
+    prepPhrase = prepPhrase.replace(" '", "'")
+    return prepPhrase
