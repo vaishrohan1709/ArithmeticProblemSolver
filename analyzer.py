dd9d2e0d6f7e702fc7945e8fec99dec8dc44d309
Janice Darling <janicedarling1@gmail.com>
2018-05-01 14:37:38 -0400

    Fixing verb

--- analyzer.py
+++ analyzer.py
@@ -85,7 +85,10 @@ def extract(word_problem):
     # Create entities for each subject
     for i, noun in enumerate(nouns):
         entity = Entity(noun, verb)
-        entity.set_value(quantities[i])
+        try:
+            entity.set_value(quantities[i])
+        except IndexError:
+            entity.set_value(0)
         try:
             entity.set_name(objects[i])
         except IndexError:
@@ -96,86 +99,4 @@ def extract(word_problem):
     nlp.close()
     return entities
 
-'''
-schema = {"put": "CHANGE_OUT", "plant": "CHANGE_OUT", "place": "CHANGE_OUT", "distribute": "CHANGE_OUT",
-          "transfer": "REDUCTION", "sell": "CHANGE_OUT", "give": "CHANGE_OUT", "add": "CHANGE_OUT",
-          "more than": "COMPARE_PLUS", "get": "CHANGE_IN", "carry": "INCREASE", "buy": "CHANGE_IN",
-          "take": "CHANGE_IN", "cut": "CHANGE_IN", "pick": "CHANGE_IN", "borrow": "CHANGE_IN",
-          "decrease": "REDUCTION", "leave": "REDUCTION", "spill": "REDUCTION", "lose": "REDUCTION",
-          "use": "REDUCTION", "spend": "REDUCTION", "saw": "REDUCTION", "eat": "REDUCTION",
-          "break": "REDUCTION", "more": "INCREASE", "build": "CHANGE_OUT", "taller": "INCREASE",
-          "load": "CHANGE_OUT", "increase": "INCREASE", "immigrate": "INCREASE", "find": "INCREASE"}
-
-
-def extract(word_problem):
-    verb = ''
-    tense = ''
-    keyword = ''
-    word_problem = word_problem.split(' . ')
-    # Create a file config.py and and set path = to path to stanford-corenlp-full-2018-02-27
-    nlp = StanfordCoreNLP(config.path, memory='8g')
-    for sentence in word_problem:
-        for word in sentence.split():
-            tag = nlp.pos_tag(word)[0][1]
-            tag = tag.encode('ascii', 'ignore')
-            if tag.startswith('V') or tag.startswith('N') or tag.startswith('R') or \
-                    tag.startswith('A') or tag.startswith('S'):
-                stem = WordNetLemmatizer().lemmatize(word, pos=tag[0].lower())
-            else:
-                stem = WordNetLemmatizer().lemmatize(word)
-            if tag.find("VB") != -1 or tag.find("RBR") != -1 or tag.find("JJ") != -1:
-                if tag.find("VB") != -1:
-                    verb = stem
-                if tag.find("VBD") != -1 or (tag.find("VBN") != -1 and tense == ''):
-                    tense = "past"
-                elif tense == '' and tag.find("VB") != -1:
-                    tense = "present"
-
-            # change made to include less than
-            if stem == "less":
-                keyword = "less than"
-            if stem in schema:
-                if not stem == "more" and stem == verb and not stem == "take":
-                    keyword = stem
-                elif stem == "take" and not sentence.find("to") != -1:
-                    keyword = stem
-                elif sentence.find("more than") != -1:
-                    keyword = "more than"
-                elif keyword == '' and stem == "take":
-                    keyword = "more"
-        print(keyword, verb, tense, sentence)
-
-        
-        # Get dependency parse for each sentence in question
-        dependencies = nlp.dependency_parse(sentence)
-        # (relation, source, target)
-        dependencies = sorted(dependencies, key=itemgetter(1))
-        get_entities(sentence, dependencies, keyword, verb, tense, nlp)
-    
-    nlp.close()
-
-
-def get_entities(sentence, dependencies, keyword, verb, tense, nlp):
-    print('sentence: ', sentence)
-    print(dependencies)
-    for edge in dependencies:
-        # get dependency tag
-        relation = edge[0]
-        target = edge[2]
-        source = edge[1]
-        print('dependency:' , relation)
-        print('source word:', sentence.split()[int(source)-1])
-        print('target word:', sentence.split()[int(target)-1])
-        tag = nlp.pos_tag(sentence.split()[int(target)-1])
-        print('tag:', tag)
-        print('\n')
-        if tag=="CD" and (relation.find("num") or relation.find("advcl") or relation.find("nsubj") or relation.find("obj")):
-            pass
-
-        newEntity = Entity()
-        newEntity.name = sentence.split()[int(source)-1]
-
-
-        print('entity name=',newEntity.name)
-'''
 
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import parser
 import analyzer
 
+schema = {"put": "CHANGE_OUT", "plant": "CHANGE_OUT", "place": "CHANGE_OUT", "distribute": "CHANGE_OUT",
+          "transfer": "REDUCTION", "sell": "CHANGE_OUT", "give": "CHANGE_OUT", "add": "CHANGE_OUT",
+          "more than": "COMPARE_PLUS", "get": "CHANGE_IN", "carry": "INCREASE", "buy": "CHANGE_IN",
+          "take": "CHANGE_IN", "cut": "CHANGE_IN", "pick": "CHANGE_IN", "borrow": "CHANGE_IN",
+          "decrease": "REDUCTION", "leave": "REDUCTION", "spill": "REDUCTION", "lose": "REDUCTION",
+          "use": "REDUCTION", "spend": "REDUCTION", "saw": "REDUCTION", "eat": "REDUCTION",
+          "break": "REDUCTION", "more": "INCREASE", "build": "CHANGE_OUT", "taller": "INCREASE",
+          "load": "CHANGE_OUT", "increase": "INCREASE", "immigrate": "INCREASE", "find": "INCREASE"}
+
 
 # Step 1: Read questions from file
 def main():
     # Load questions from file
     with open('data/q1.txt', 'r') as fi:
         questions = fi.readlines()
-
-        word_problem = parser.parse(questions[12])
+        # Step 2: Simplify the question by resolving conjunctions
+        word_problem = parser.parse(questions[13])
         print(word_problem)
+        # Step 3 : Extract entities from the question
         entities = analyzer.extract(word_problem)
+        # Step 4: categorize questions based on verb and schema
         for entity in entities:
-            print(entity.name, entity.owner, entity.value, entity.verb)
+            pass
+            print(entity.owner, entity.name, entity.value, entity.verb)
 
 
 if __name__ == '__main__':
